328baa499b7b2013f8b792832d5da83f2e7e34bb
JosuaHall <halljosua@web.de>
2024-04-24 18:38:44 -0500

    thrid commit

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const challanges = require("./routes/api/challanges");
 
 const app = express();
 
-const corsOptions = {
+/*const corsOptions = {
   origin: ["http://localhost:3000", "*"],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+app.use(cors(corsOptions));*/
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
