c5aaf677475a61ae4f1b2e571509475f221a1d14
JosuaHall <halljosua@web.de>
2024-04-25 09:10:41 -0500

    cors deleted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
-const cors = require("cors");
+//const cors = require("cors");
 
 const challanges = require("./routes/api/challanges");
 
